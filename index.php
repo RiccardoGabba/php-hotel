9a0ff32b09f62c3d5e7ec3a9b4155f541afb4fec
filippo <riccardo.gabba02@gmail.com>
2023-11-30 15:28:03 +0100

    initialized the project

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+include __DIR__ ."/partials/header.php";
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>index</h2>
+</body>
+
+<?php 
+include __DIR__ ."/partials/footer.php";
+?>
+</html>
