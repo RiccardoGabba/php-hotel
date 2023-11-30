398b7a1f4d46f1a19e67ee422886391d79b60f63
filippo <riccardo.gabba02@gmail.com>
2023-11-30 15:40:23 +0100

    i made the foreach to make me able to print everything from the array

--- index.php
+++ index.php
@@ -1,21 +1,38 @@
-<?php 
-include __DIR__ ."/partials/header.php";
-
+<?php
+include __DIR__ . "/partials/header.php";
+include __DIR__ . "./model/db.php";
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    <h2>index</h2>
+
+<body class="container">
+    <h2>Elenco hotel</h2>
+    <div>
+        <?php foreach ($hotels as $hotels) { ?>
+            <table class="table">
+                <tbody>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col"><?php echo $hotels['name'] ?></th>
+                        <th scope="col">Cognome</th>
+                        <th scope="col">Username</th>
+                    </tr>
+                </tbody>
+            <?php } ?>
+    </div>
+
 </body>
 
-<?php 
-include __DIR__ ."/partials/footer.php";
+<?php
+include __DIR__ . "/partials/footer.php";
 ?>
+
 </html>
